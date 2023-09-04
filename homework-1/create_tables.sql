cb52bb5fe8298e93409b32b2ffe7962fc20d3634
Vasilii Kuptsov <kuptsovy@mail.ru>
2023-09-04 15:31:56 +0300

    hw1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
--- SQL-команды для создания таблиц
+CREATE DATABASE north
+	WITH
+	OWNER = postgres
+	ENCODING = 'Utf-8'
+	CONNECTION LIMIT = -1
+	IS_TEMPLATE = False;
+CREATE TABLE customers
+(
+	customer_id text PRIMARY KEY,
+	company_name text,
+	contact_name text
+);
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name text,
+	last_name text,
+	title text,
+	birth_date date,
+	notes text
+);
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id text,
+	employee_id int,
+	order_date date,
+	ship_city text
+);- SQL-команды для создания таблиц
