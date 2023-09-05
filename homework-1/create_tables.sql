5f82a419a9ae70aab6a484ba345055cb742fb038
Vasilii Kuptsov <kuptsovy@mail.ru>
2023-09-05 13:56:54 +0300

    hw11

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -6,24 +6,24 @@ CREATE DATABASE north
 	IS_TEMPLATE = False;
 CREATE TABLE customers
 (
-	customer_id text PRIMARY KEY,
-	company_name text,
-	contact_name text
+	customer_id varchar(10) PRIMARY KEY,
+	company_name varchar(40),
+	contact_name varchar(30)
 );
 CREATE TABLE employees
 (
 	employee_id int PRIMARY KEY,
-	first_name text,
-	last_name text,
-	title text,
+	first_name varchar(10),
+	last_name varchar(20),
+	title varchar(30),
 	birth_date date,
 	notes text
 );
 CREATE TABLE orders
 (
 	order_id int PRIMARY KEY,
-	customer_id text,
-	employee_id int,
+	customer_id varchar(5) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
 	order_date date,
-	ship_city text
-);- SQL-команды для создания таблиц
+	ship_city varchar(15)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -3,7 +3,7 @@ import psycopg2
 import csv
 path = 'north_data/customers_data.csv'
 conn = psycopg2.connect(host="localhost", database="north", user="postgres", port=5433, password="1705")
-cursor = conn.cursor()
+curs = conn.cursor()
 
 with open(path, 'r') as csvfile:
     reader = csv.reader(csvfile)
@@ -11,7 +11,7 @@ with open(path, 'r') as csvfile:
         if row[0] != 'customer_id':
             formula = "INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)"
             values =(row[0], row[1], row[2])
-            cursor.execute(formula, values)
+            curs.execute(formula, values)
 path = 'north_data/employees_data.csv'
 with open(path, 'r') as csvfile:
     reader = csv.reader(csvfile)
@@ -19,16 +19,19 @@ with open(path, 'r') as csvfile:
         if row[0] != 'employee_id':
             formula = "INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s, %s)"
             values =(row[0], row[1], row[2], row[3], row[4], row[5])
-            cursor.execute(formula, values)
+            curs.execute(formula, values)
+
 path = 'north_data/orders_data.csv'
 with open(path, 'r') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         if row[0] != 'order_id':
             formula = "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)"
-            values =(row[0], row[1], row[2], row[3], row[4])
-            cursor.execute(formula, values)
+            values = (row[0], row[1], row[2], row[3], row[4])
+            curs.execute(formula, values)
 conn.commit()
+curs.close()
+conn.close()
 
 
 
